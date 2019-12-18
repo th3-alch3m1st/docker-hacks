e2cbeb17894da96421d2934f8c4f1346af25c847
th3-alch3m1st <an1sor0pous@protonmail.com>
2019-12-18 13:13:02 +0000

    added start.sh

---
+++ passive-recon/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat /tools/input/domain.txt
+
+#while read -r line; do
+#    echo "$line" | dnsgen - > /tools/output/dnsgen.$line.$(date +%Y%m%d%H%M%S)
+#done < /tools/input/domain.txt
